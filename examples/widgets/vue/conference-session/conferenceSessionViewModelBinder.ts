e93f4558c08d2db32bd35c342ef1b2b5f763ee2d
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2022-03-28 13:05:31 -0700

    tmp

--- examples/widgets/vue/conference-session/conferenceSessionViewModelBinder.ts
+++ examples/widgets/vue/conference-session/conferenceSessionViewModelBinder.ts
@@ -1,6 +1,6 @@
 import { Bag } from "@paperbits/common";
-import { EventManager, Events } from "@paperbits/common/events";
-import { ComponentFlow, IWidgetBinding, WidgetBinding } from "@paperbits/common/editing";
+import { EventManager } from "@paperbits/common/events";
+import { ComponentFlow, WidgetBinding } from "@paperbits/common/editing";
 import { ViewModelBinder } from "@paperbits/common/widgets";
 import { ConferenceSession } from "./design/conferenceSession";
 import { widgetName, widgetDisplayName, widgetEditorSelector } from "./constants";
@@ -10,36 +10,7 @@ import { ConferenceSessionModel } from "./conferenceSessionModel";
 export class ConferenceSessionViewModelBinder implements ViewModelBinder<ConferenceSessionModel, ConferenceSession>  {
     constructor(private readonly eventManager: EventManager) { }
 
-    // public async updateViewModel(model: ConferenceSessionModel, viewModel: ConferenceSession): Promise<void> {
-    //     viewModel.runtimeConfig({ sessionNumber: model.sessionNumber });
-    // }
-
-    // public async modelToViewModel(model: ConferenceSessionModel, viewModel?: ConferenceSession, bindingContext?: Bag<any>): Promise<ConferenceSession> {
-    //     if (!viewModel) {
-    //         viewModel = new ConferenceSession();
-
-    //         const binding: IWidgetBinding<ConferenceSessionModel, ConferenceSession> = {
-    //             name: widgetName,
-    //             displayName: widgetDisplayName,
-    //             readonly: bindingContext?.readonly,
-    //             model: model,
-    //             draggable: true,
-    //             flow: ComponentFlow.Block,
-    //             editor: widgetEditorSelector,
-    //             applyChanges: async () => {
-    //                 await this.updateViewModel(model, viewModel);
-    //                 this.eventManager.dispatchEvent(Events.ContentUpdate);
-    //             }
-    //         };
-    //         viewModel["widgetBinding"] = binding;
-    //     }
-
-    //     this.updateViewModel(model, viewModel);
-
-    //     return viewModel;
-    // }
-
-    public async createWidgetBinding(model: ConferenceSessionModel, bindingContext: Bag<any>): Promise<WidgetBinding<ConferenceSessionModel, ConferenceSession>> {
+    public async createWidgetBinding(model: ConferenceSessionModel): Promise<WidgetBinding<ConferenceSessionModel, ConferenceSession>> {
         const binding = new WidgetBinding<ConferenceSessionModel, ConferenceSession>();
         binding.framework = "vue";
         binding.model = model;
@@ -51,17 +22,17 @@ export class ConferenceSessionViewModelBinder implements ViewModelBinder<Confere
         binding.draggable = true;
         binding.viewModelClass = ConferenceSession;
         binding.applyChanges = async () => {
-            await this.modelToViewModel(model, binding.viewModel, bindingContext);
+            await this.modelToViewModel(model, binding.viewModel);
             this.eventManager.dispatchEvent("onContentUpdate");
         };
         binding.onCreate = async () => {
-            await this.modelToViewModel(model, binding.viewModel, bindingContext);
+            await this.modelToViewModel(model, binding.viewModel);
         };
 
         return binding;
     }
 
-    public async modelToViewModel(model: ConferenceSessionModel, viewModel: ConferenceSession, bindingContext?: Bag<any>): Promise<ConferenceSession> {
+    public async modelToViewModel(model: ConferenceSessionModel, viewModel: ConferenceSession): Promise<ConferenceSession> {
         viewModel.sessionNumber = model.sessionNumber;
         return viewModel;
     }
